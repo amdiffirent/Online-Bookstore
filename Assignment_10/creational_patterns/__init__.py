d5971bf38564b3278441ec665dce2c8800c7a8ea
Akhona <128717724+amdiffirent@users.noreply.github.com>
2025-04-26 19:15:55 +0200

    run tests.

--- Assignment_10/creational_patterns/__init__.py
+++ Assignment_10/creational_patterns/__init__.py
@@ -12,17 +12,3 @@ __all__ = [
 ]
 
 
-
-
-'''# Assignment_10/creational_patterns/__init__.py
-# Keep this minimal - don't try to import things that don't exist yet
-# Can be empty or just contain:
-__all__ = ['abstract_factory', 'builder', 'factory_method', 'prototype', 'simple_factory', 'singleton']
-
-
-from .simple_factory import BookFactory
-from .factory_method import PaymentProcessorFactory, CreditCardProcessor, PayPalProcessor
-from .abstract_factory import WindowsFactory, MacOSFactory
-from .builder import BookBuilder
-from .prototype import Book as PrototypeBook, ShapeCache
-from .singleton import DatabaseConnection'''
